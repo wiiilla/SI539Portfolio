aca0b4d7e4650fcfe4f974dbf936718fb4a49ac9
Willa Hua <bhua@umich.edu>
2020-11-09 22:33:07 -0500

    progress checkpoint

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+// Expand Nav Bar
+function expand_nav() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
